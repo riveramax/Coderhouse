db7f7d22533a2f15bb7742fd4147060e7f7ae274
riveramax <maxi.riveram@gmail.com>
2022-05-28 21:15:39 -0300

    commit
    28/05 - 21:15hrs

---
+++ Mi_primer_MVT/Primer_MVT/Primer_MVT/views.py
@@ -0,0 +1,21 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def familiar_1 (request):
+    return HttpResponse ('F_1')
+
+def Fam_1(request):
+    context = {
+        'nombre':'Maxi',
+        'apellido':'Rivera',
+        'prueba':'1234',
+        'frutas':['asd', 'dfgdfg', '243423423']
+    }
+    return render(request, 'Template_1.html', context = context)
+
+
+
+
+
+
---
+++ Mi_primer_MVT/Primer_MVT/Readme.txt
@@ -0,0 +1,9 @@
+
+# Descarga:
+    - 
+
+# Instalacion:
+    - pip install -r requirements.txt
+    - python manage.py migrate
+    - python manage.py runserver
+
